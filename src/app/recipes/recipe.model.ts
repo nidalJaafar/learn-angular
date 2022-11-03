4e43324ff8b0f2893920a4e8f1e7b38195ece080
nidal jaafar <nidaljaafar.nj@gmail.com>
2022-11-03 21:38:02 +0200

    added recipe and shopping components

---
+++ src/app/recipes/recipe.model.ts
@@ -0,0 +1,4 @@
+export class Recipe {
+  constructor(public readonly name: string, public readonly description: string, public readonly imagePath: string) {
+  }
+}
---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,4 @@
+export class Ingredient {
+  constructor(public readonly name: string, public readonly amount: number) {
+  }
+}
